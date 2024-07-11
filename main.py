573f9e77b76ed2812f5474c3a50d4ce25d672bcf
sato wataru <watarusato105@gmail.com>
2024-07-11 14:29:34 +0900

    #1 ルーティング

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 import flask
-import functions_framework
+from flask import Request, request
+import werkzeug.datastructures
 
-@functions_framework.http
-def hello(request: flask.Request) -> flask.typing.ResponseReturnValue:
-    return "Hello world!"
+
+app = flask.Flask(__name__)
+
+## ルーティング
+@app.route("/test", methods=["GET"])
+def test():
+  return "hello test!"
+
+
+@app.route("/fetch", methods=['POST'])
+def fetch():
+  return "hello " + request.path
+
+
+def main(request: Request):
+  with app.app_context():
+    headers = werkzeug.datastructures.Headers()
+    for key, value in request.headers.items():
+      headers.add(key, value)
+    with app.test_request_context(method=request.method, base_url=request.base_url, path=request.path, query_string=request.query_string, headers=headers, data=request.data):
+      try:
+        rv = app.preprocess_request()
+        if rv is None:
+          rv = app.dispatch_request()
+      except Exception as e:
+        rv = app.handle_user_exception(e)
+      response = app.make_response(rv)
+      return app.process_response(response)
